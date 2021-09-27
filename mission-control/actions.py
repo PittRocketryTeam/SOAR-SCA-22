de6c9bf97b212bd6ab4e01873fb575756225813c
Patrick Roche <rochep63@gmail.com>
2021-09-27 10:44:36 -0400

    add action templates

--- mission-control/actions.py
+++ mission-control/actions.py
@@ -1,2 +1,34 @@
 #!/usr/bin/python3
 
+def mode_idle_cb():
+    pass
+
+def mode_startup_cb():
+    pass
+
+def mode_go_cb():
+    pass
+
+def log_start_cb():
+    pass
+
+def log_stop_cb():
+    pass
+
+def log_reset_cb():
+    pass
+
+def payload_arm_cb():
+    pass
+
+def payload_disarm_cb():
+    pass
+
+def payload_safe_cb():
+    pass
+
+def command_cb(sv):
+    pass
+
+def command_send_cb(event):
+    pass
--- mission-control/commander.py
+++ mission-control/commander.py
@@ -35,37 +35,40 @@ class Commander(Service):
         # button panel
 
         # mode switch
-        self.mode_idle_btn = tk.Button(self.root, text="Mode Idle", anchor=tk.W)
+        self.mode_idle_btn = tk.Button(self.root, text="Mode Idle", anchor=tk.W, command=actions.mode_idle_cb)
         self.mode_idle_btn.grid(row=0, column=0, sticky=tk.E+tk.W, padx=2, pady=2,)
 
-        self.mode_startup_btn = tk.Button(self.root, text="Mode Startup", anchor=tk.W)
+        self.mode_startup_btn = tk.Button(self.root, text="Mode Startup", anchor=tk.W, command=actions.mode_startup_cb)
         self.mode_startup_btn.grid(row=1, column=0, sticky=tk.E+tk.W, padx=2, pady=2,)
 
-        self.mode_go_btn = tk.Button(self.root, text="Mode Go", anchor=tk.W)
+        self.mode_go_btn = tk.Button(self.root, text="Mode Go", anchor=tk.W, command=actions.mode_go_cb)
         self.mode_go_btn.grid(row=2, column=0, sticky=tk.E+tk.W, padx=2, pady=2,)
 
         # logging ctrl
-        self.log_start_btn = tk.Button(self.root, text="Log Start", anchor=tk.W)
+        self.log_start_btn = tk.Button(self.root, text="Log Start", anchor=tk.W, command=actions.log_start_cb)
         self.log_start_btn.grid(row=0, column=1, sticky=tk.E+tk.W, padx=2, pady=2,)
 
-        self.log_stop_btn = tk.Button(self.root, text="Log Stop", anchor=tk.W)
+        self.log_stop_btn = tk.Button(self.root, text="Log Stop", anchor=tk.W, command=actions.log_stop_cb)
         self.log_stop_btn.grid(row=1, column=1, sticky=tk.E+tk.W, padx=2, pady=2,)
 
-        self.log_reset_btn = tk.Button(self.root, text="Log Reset", anchor=tk.W)
+        self.log_reset_btn = tk.Button(self.root, text="Log Reset", anchor=tk.W, command=actions.log_reset_cb)
         self.log_reset_btn.grid(row=2, column=1, sticky=tk.E+tk.W, padx=2, pady=2,)
 
         # payload ctrl
-        self.payload_arm_btn = tk.Button(self.root, text="Payload Arm", anchor=tk.W)
+        self.payload_arm_btn = tk.Button(self.root, text="Payload Arm", anchor=tk.W, command=actions.payload_arm_cb)
         self.payload_arm_btn.grid(row=0, column=2, sticky=tk.E+tk.W, padx=2, pady=2,)
 
-        self.payload_disarm_btn = tk.Button(self.root, text="Payload Disarm", anchor=tk.W)
+        self.payload_disarm_btn = tk.Button(self.root, text="Payload Disarm", anchor=tk.W, command=actions.payload_disarm_cb)
         self.payload_disarm_btn.grid(row=1, column=2, sticky=tk.E+tk.W, padx=2, pady=2,)
 
-        self.payload_safe_btn = tk.Button(self.root, text="Payload Safe", anchor=tk.W)
+        self.payload_safe_btn = tk.Button(self.root, text="Payload Safe", anchor=tk.W, command=actions.payload_safe_cb)
         self.payload_safe_btn.grid(row=2, column=2, sticky=tk.E+tk.W, padx=2, pady=2,)
 
         # command line
-        self.cmd_entry = tk.Entry(self.root, width=20)
+        self.sv = tk.StringVar()
+        self.sv.trace("w", lambda name, index, mode, sv=self.sv: actions.command_cb(self.sv))
+        self.cmd_entry = tk.Entry(self.root, width=20, textvariable=self.sv)
+        self.cmd_entry.bind("<Return>", actions.command_send_cb)
         self.cmd_entry.grid(row=3, column=1, columnspan=2, padx=2, pady=2, sticky=tk.E+tk.W)
 
         # status label
