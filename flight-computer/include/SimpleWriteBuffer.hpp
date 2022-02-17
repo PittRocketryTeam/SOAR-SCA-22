012dc4f730cc60f9d24ef7e3df68de6b96b24632
Patrick Roche <pmr25@pitt.edu>
2022-02-17 16:07:04 -0500

    implement vector based writebuffer

--- flight-computer/include/SimpleWriteBuffer.hpp
+++ flight-computer/include/SimpleWriteBuffer.hpp
@@ -2,12 +2,17 @@
 #define SIMPLEWRITEBUFFER_HPP
 
 #include "WriteBufferInterface.h"
+#include <cstdint>
+#include <vector>
+
+#define WB_MAX_SIZE 8192
 
 class SimpleWriteBuffer : public EmbeddedProto::WriteBufferInterface
 {
 public:
     SimpleWriteBuffer()
     {
+        mBuffer.reserve(WB_MAX_SIZE);
     }
 
     virtual ~SimpleWriteBuffer() = default;
@@ -19,6 +24,9 @@ public:
 
     virtual bool push(const uint8_t);
     virtual bool push(const uint8_t*, const uint32_t);
+
+private:
+    std::vector<uint8_t> mBuffer;
 };
 
 #endif /* SIMPLEWRITEBUFFER_HPP */
--- flight-computer/src/SimpleWriteBuffer.cpp
+++ flight-computer/src/SimpleWriteBuffer.cpp
@@ -2,30 +2,47 @@
 
 void SimpleWriteBuffer::clear()
 {
-
+    mBuffer.clear();
 }
 
 uint32_t SimpleWriteBuffer::get_size() const
 {
-    return 0;
+    return (uint32_t)mBuffer.size();
 }
 
 uint32_t SimpleWriteBuffer::get_max_size() const
 {
-    return 0;
+    return mBuffer.max_size();
 }
 
 uint32_t SimpleWriteBuffer::get_available_size() const
 {
-    return 0;
+    return get_max_size() - get_size();
 }
 
 bool SimpleWriteBuffer::push(const uint8_t byte)
 {
+    if (mBuffer.size() >= get_max_size())
+    {
+        return false;
+    }
+
+    mBuffer.push_back(byte);
+
     return true;
 }
 
 bool SimpleWriteBuffer::push(const uint8_t* buf, const uint32_t len)
 {
+    if (mBuffer.size() + len >= get_max_size())
+    {
+        return false;
+    }
+
+    for (uint32_t i = 0; i < len; ++i)
+    {
+        mBuffer.push_back(buf[i]);
+    }
+
     return true;
 }
